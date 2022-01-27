0e7c335c9107024130a957f2aee47826ad91294f
Riccardo Bertolini <info@riccardobertolini.net>
2022-01-27 22:38:33 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,88 @@
+function hideErrors () {
+    const allErrors = Array.from(document.querySelectorAll('.field-error')).forEach(error => !error.classList.contains('hidden') && error.classList.add('hidden'));
+}
+
+function showErrors(errorsList) {
+  errorsList.forEach(failingField => {
+      document.getElementById(failingField).classList.remove('hidden');
+    });
+}
+
+function validateForm(event) {
+  hideErrors();
+  const fields = document.querySelectorAll('.form-input');
+  const errors = Array.from(fields).map(field => validateField(field)).filter(item => item);
+  showErrors(errors);
+  return false;
+}
+
+const validateText = (value) => {
+  const rules = /[A-Za-z0-9 _]/g;
+  return rules.test(value);
+};
+
+const validatePhone = (value) => {
+  const rules = /^1?[\(\- ]*\d{3}[\)-\. ]*\d{3}[-\. ]*\d{4}$/g;
+  return rules.test(value);
+};
+
+const validateNumber = (value) => {
+  const rules = /^[0-9]*$/;
+  return rules.test(value);
+};
+
+const validateEmail = (value) => {
+  const rules = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/g;
+  return rules.test(value);
+};
+
+const validateDate = (value) => {
+  const rules = /^[0-9-.\/]$/g;
+  return rules.test(value);
+};
+
+
+const isEmpty = (value) => value === '' || value === undefined || value.length === 0;
+
+const isMultiFieldEmpty = (field) => {
+  return !Array.from(document.getElementsByName(field.name)).some(field => field.checked);
+};
+
+const getBoolean = (value) => value === 'true';
+
+const validateField = (field) => {
+const isRequired = getBoolean(field?.attributes['data-required'].value);
+const isMultiField = field.type === 'radio' || field.type === 'checkbox';
+  
+  if(isRequired && isMultiField && isMultiFieldEmpty(field)) { 
+    return `${field.name}-error-1`;
+  }
+  
+  if(isRequired && isEmpty(field.value)) {
+    return `${field.name}-error-1`;
+  }
+  if(!isEmpty(field.value)) {
+    switch(field.type){
+        case 'text':
+          return validateText(field.value) ? null : `${field.name}-error-2`;
+          break;
+        case 'email':
+          return validateEmail(field.value) ? null : `${field.name}-error-2`;
+          break;
+        case 'tel':
+          return validatePhone(field.value) ? null : `${field.name}-error-2`;
+          break;
+        case 'number':
+          return validateNumber(field.value) ? null : `${field.name}-error-2`;
+          break;
+        case 'date':
+          return validateDate(field.value) ? null : `${field.name}-error-2`;
+          break;
+    }
+  }
+}
+
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+
+//   }); 
