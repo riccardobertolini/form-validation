c0d6acb1304ed0b7afca3022851b146bb00d30ef
Riccardo Bertolini <info@riccardobertolini.net>
2022-01-27 23:24:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,8 @@ const validateNumber = (value) => {
 };
 
 const validateEmail = (value) => {
-  const rules = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  //const rules = /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  const rules = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return rules.test(value);
 };
 
